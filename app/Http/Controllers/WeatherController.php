95a4279e71f0da198a7b16a8701ec966843df8b3
Milton-R <ramalhom@aston.ac.uk>
2020-07-07 03:40:15 +0100

    grab location from api

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -33,22 +33,47 @@ class WeatherController extends Controller
 
     public function index()
     {
-        $client = new Client();
-        $response = $client->request('GET', 'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=66558235d392bd37f58293db6dd6f887');
-        $statusCode = $response->getStatusCode();
-//        $body = $response->getBody()->getContents();
-        $body = json_decode($response->getBody(), true);
+        $apikey='7420a840598c41eb94321709202706';
+        $paa='http://api.worldweatheronline.com/premium/v1/weather.ashx?key='.$apikey.'&q=London&num_of_days=7&tp=24&mca=no&show_comments=no&format=json';
 
-        $array = $response->getBody();
-        $json = json_decode($array, true);
-        $collection = collect($json);
-        $access = $collection->get('main');
+        $loc =new Client;
+        $loccode= $loc ->request('GET', 'https://www.metaweather.com/api/location/search/?query=london');
+        $locbod=$loccode->getBody();
+        $localss = json_decode($locbod,true);
+        $v = collect($localss);
+        var_dump($localss[0]['woeid']);
 
-        $c = collect($access);
-        $tem = $c->get('temp_min');
+//        $client = new Client();
+//        $response = $client->request('GET', 'https://www.metaweather.com/api/location/'.$localss.'/');
+//        $statusCode = $response->getStatusCode();
+////        $body = $response->getBody()->getContents();
+//        $body = json_decode($response->getBody(), true);
+//
+//        $array = $response->getBody();
+//        $json = json_decode($array);
+//        $d=collect($json->consolidated_weather);
+//
+//        //$c = collect($access->weather);
+//
+//        $hottemp=0;
+//
+//        foreach ($d as $forecast){
+//           // $for = $forecast->avgtempC;
+//            ($forecast -> weather_state_name);
+//
+////            if ($forecast->avgtemp > 20){
+////                    $hottemp=$hottemp+1;
+////            }
+//
+//        }
+//
+//
 
-        dd($collection);
-//        dd($access);
+
+
+    }
+
+    public function weathersuggestion($something){
 
 
     }
